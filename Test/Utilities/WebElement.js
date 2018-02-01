bf0ae199ff504a9e3d0eb9cbbcbd74d37ebc15f1
rohit saini <rohit.saini.uk@gmail.com>
2018-02-01 11:35:17 +0530

    Utlitlities folder

--- Test/Utilities/WebElement.js
+++ Test/Utilities/WebElement.js
@@ -1,9 +1,1 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var WebElement = /** @class */ (function () {
-    function WebElement() {
-    }
-    return WebElement;
-}());
-exports.WebElement = WebElement;
 //# sourceMappingURL=WebElement.js.map
---
+++ Test/Utilities/WebElementLocator.ts
@@ -0,0 +1,25 @@
+import { element, by, By } from 'protractor';
+
+export class WebElementLocator {
+
+    static async  getOptions() {
+        return element.all(by.tagName('option'));
+    }
+
+    static async selectByValue(value) {
+        return element.all(by.css('option[value="' + value + '"]')).click();
+    }
+
+    static async getSelectedOptions() {
+        return element.all(by.css('option[selected="selected"]'));
+    }
+
+    static async selectByPartialText(text) {
+        return element.all(by.cssContainingText('option', text)).click();
+    }
+
+    static async selectByText(text) {
+        return element.all(by.xpath('option[.="' + text + '"]')).click();
+    }
+
+}
